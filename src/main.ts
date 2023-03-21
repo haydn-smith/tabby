b4a6125cc7fcf9b548e1ac5e0d736ba2ebb65ad7
Haydn Smith <h.smith94@gmail.com>
2023-03-21 19:19:31 +1030

    remove store

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
-import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import App from './App.vue';
 import './style.css';
 
-createApp(App).use(createPinia()).mount('#app');
+createApp(App).mount('#app');
