0c259f9d5682712b9828d3bc2bbdba5e30dbf83e
Haydn Smith <h.smith94@gmail.com>
2023-03-21 21:08:54 +1030

    controls (#2)

--- src/components/TabbyColumn.vue
+++ src/components/TabbyColumn.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ArrowSmallLeftIcon, ArrowSmallRightIcon, XMarkIcon } from '@heroicons/vue/24/solid';
 import Column from '../values/column';
 import Cursor from '../values/cursor';
 import TabbyNote from './TabbyNote.vue';
@@ -16,6 +17,9 @@ const props = withDefaults(
 );
 
 const emits = defineEmits<{
+  (e: 'columnDeleted'): void;
+  (e: 'insertColumnBefore'): void;
+  (e: 'insertColumnAfter'): void;
   (e: 'columnChanged', column: Column): void;
   (e: 'noteSelected', string: number, active: boolean): void;
 }>();
@@ -27,10 +31,14 @@ const onNoteChanged = (string: number, fret: string) => {
 const onNoteSelected = (string: number, active: boolean) => {
   emits('noteSelected', string, active);
 };
+
+const onDeleteClicked = () => {
+  emits('columnDeleted');
+};
 </script>
 
 <template>
-  <div :class="{ 'text-blue-400': !column, 'bg-blue-100': isSelected && !isReadOnly }">
+  <div class="relative" :class="{ 'text-blue-400': !column, 'bg-blue-100': isSelected && !isReadOnly }">
     <div v-for="string in column.getStrings()" :key="string">
       <TabbyNote
         @note-selected="(active) => onNoteSelected(string, active)"
@@ -41,6 +49,26 @@ const onNoteSelected = (string: number, active: boolean) => {
         :is-read-only="isReadOnly"
         :note="column.getNoteForString(string)"
       />
+    </div>
+    <div v-if="isSelected && !isReadOnly" class="absolute left-0 right-0 flex h-4">
+      <div
+        @click="emits('insertColumnBefore')"
+        class="absolute right-full flex h-4 w-4 cursor-pointer items-center justify-center rounded-l bg-blue-200 hover:bg-blue-300"
+      >
+        <ArrowSmallLeftIcon class="h-4" />
+      </div>
+      <div
+        @click="onDeleteClicked"
+        class="absolute inset-0 flex h-4 cursor-pointer items-center justify-center bg-blue-200 hover:bg-blue-300"
+      >
+        <XMarkIcon class="h-4" />
+      </div>
+      <div
+        @click="emits('insertColumnAfter')"
+        class="absolute left-full flex h-4 w-4 cursor-pointer items-center justify-center rounded-r bg-blue-200 hover:bg-blue-300"
+      >
+        <ArrowSmallRightIcon class="h-4" />
+      </div>
     </div>
   </div>
 </template>
