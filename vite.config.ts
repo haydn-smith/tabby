b944ff50b6c7645fd028254806ffc7ef20c2c101
Haydn Smith <h.smith94@gmail.com>
2023-03-25 15:54:19 +1030

    analytics

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePluginRadar({
+      analytics: {
+        id: 'G-6BT0P40NLF',
+      },
+    })
+  ],
   base: '/tabby/',
 })
